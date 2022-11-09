68bc8c7cf64fbcc1e1b75aae5704046caebd82b2
thiago <201908700904@alunos.estacio.br>
2022-11-09 16:58:03 -0300

    finalizando projeto
    código alterado por completo

--- checkpoint/projeto1.js
+++ checkpoint/projeto1.js
@@ -1,59 +1,31 @@
-let readlineSync = require("readline-sync");
+const pratos = {
+  1: { nome: "Pipoca", tempo: 10 },
+  2: { nome: "Macarrão", tempo: 8 },
+  3: { nome: "Carne", tempo: 15 },
+  4: { nome: "Feijão", tempo: 12 },
+  5: { nome: "Brigadeiro", tempo: 8 },
+};
 
-console.log(`
-Selecione seu prato: 
+function microondas(numeroPrato, tempo) {
+  const prato = pratos[numeroPrato];
 
-1 - pipoca
-2 - macarrao
-3 - carne
-4 - feijao
-5 - brigadeiro
-`);
-
-let opcao = readlineSync.question("escolha um item: ");
-console.log("A opcao escolhida foi: " + opcao);
-
-let tempo = readlineSync.question("digite o tempo: ");
-console.log("tempo escolhido " + tempo);
-
-switch (opcao) {
-  case "1":
-    console.log("Escolheu pipoca");
-    break; // Encerrar/Terminar a execução do switch
-  case "2":
-    console.log("Esolheu macarrao");
-    break;
-  case "3":
-    console.log("Escolheu carne");
-    break;
-  case "4":
-    console.log("Escolheu feijao");
-    break;
-  case "5":
-    console.log("Escolheu brigadeiro");
-    break;
-  default:
-    console.log("Opção inválida");
-}
-
-let pipoca = 10;
-let macarrao = 8;
-let carne = 15;
-let feijao = 12;
-let brigadeiro = 8;
-
-function microondas(opcao, tempo) {
-  if (tempo != 0) {
-    if (tempo > 2 * opcao) {
-      console.log("comida queimou");
-    } else if (tempo < opcao) {
-      console.log("tempo insuficiente");
-    } else if (tempo >= 3 * opcao) {
-      console.log("kabummm!!!");
-    }
+  if (!prato) {
+    return console.log("Prato inexistente");
   }
 
-  console.log("BIP BIP");
+  let mensagem;
+  const tempoPadrao = prato.tempo;
+
+  if (tempo < tempoPadrao) {
+    mensagem = "Tempo insuficiente";
+  } else if (tempo > tempoPadrao * 2 && tempo < tempoPadrao * 3) {
+    mensagem = "A comida queimou";
+  } else if (tempo >= tempoPadrao * 3) {
+    mensagem = "Kabummm";
+  }
+
+    console.log("BIP,BIP !!!");
+    return console.log(mensagem);
 }
 
-microondas(opcao, tempo);
+microondas(1, 17);
