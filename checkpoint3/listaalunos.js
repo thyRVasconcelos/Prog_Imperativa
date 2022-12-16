b35c4b4bd06626d542847e153c7049738a329b6a
Thyago Vasconcelos <201908700904@alunos.estacio.br>
2022-12-15 22:06:40 -0300

    agora vai

--- checkpoint3/listaalunos.js
+++ checkpoint3/listaalunos.js
@@ -6,6 +6,6 @@ listaAlunos.push(new cadastro2.cadastro("Breno", 0, [9, 10, 10]));
 listaAlunos.push(new cadastro2.cadastro("Inacio", 3, [10, 9.75, 10]));
 listaAlunos.push(new cadastro2.cadastro("Amanda", 1, [10, 10, 10]));
 listaAlunos.push(new cadastro2.cadastro("Fabiola", 3, [8, 7, 10]));
-listaAlunos.push(new cadastro2.cadastro("Thiago", 1, [8, 8, 7]));
+listaAlunos.push(new cadastro2.cadastro("Lucas", 1, [8, 8, 7]));
 
 module.exports = listaAlunos;
